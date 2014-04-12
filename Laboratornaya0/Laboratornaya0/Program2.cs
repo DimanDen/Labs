c300fc4bdc48cb1feac57bebd743aa02fd45b14a
DimanDen <diman.mymail@rambler.ru>
2014-04-12 17:09:29 +0400

    1

--- Laboratornaya0/Laboratornaya0/Program2.cs
+++ Laboratornaya0/Laboratornaya0/Program2.cs
@@ -6,13 +6,17 @@ using System.Threading.Tasks;
 
 namespace Objects1
 {
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of a73e55a... 1
     interface IDraw
     {
         void Draw(); //method to display the objects
     }
 
 
+<<<<<<< HEAD
     class Points
     {
         public int b = 0; //auxiliary variables to create three different triangles
@@ -28,10 +32,21 @@ namespace Objects1
                 x[i] = x[i - 1] + 10; 
                 y[i] = y[i - 1] + 5;
             }
+=======
+    class Point
+    {
+        public int x; //coordinates of points
+        public int y;
+        public Point(int x, int y) //constructor with a pointer to the object
+        {
+            this.x = x;
+            this.y = y;
+>>>>>>> parent of a73e55a... 1
         }
     }
 
 
+<<<<<<< HEAD
     class Polygon : Points, IDraw
     {
 
@@ -45,6 +60,37 @@ namespace Objects1
                 Console.Write("\nx = {0}", point.x[i] + b); //output points of the polygon
                 Console.Write("\ny = {0}", point.y[i] + c); 
                 Console.Write("\n------------------------------------"); //separation
+=======
+    class Polygon : IDraw
+    {
+        protected Point[] Points; //array of points
+        protected int heaps; //the number of vertices
+        public int Heaps //properties, which specifies the conditions the number of vertices
+        {
+            get
+            {
+                heaps = Points.Length;
+                return heaps; 
+            }
+            set
+            {   }
+        }
+
+        protected Polygon()
+        {
+        }
+
+        public Polygon(Point[] Points) //constructor with a pointer to the object
+        {
+            this.Points = Points;
+        }
+        
+        public void Draw() //method which output points to the screen
+        {
+            for (int i = 0; i < Points.Length; i++)
+            {
+                Console.WriteLine("x = {0}; y = {1}", Points[i].x, Points[i].y);
+>>>>>>> parent of a73e55a... 1
             }
         }
     }
@@ -52,6 +98,7 @@ namespace Objects1
 
     class Triangle : Polygon
     {
+<<<<<<< HEAD
 
         public void Draw()
         {
@@ -62,6 +109,13 @@ namespace Objects1
                 Console.Write("\nx = {0}", point.x[i] + 11 + b); //output points of the triangle
                 Console.Write("\ny = {0}", point.y[i] + 23 + c); 
                 Console.Write("\n------------------------------------"); //separation
+=======
+        public Triangle(Point[] Points)
+        {
+            if (Points != null && Points.Length == 3) //Condition for the vertices of the triangle
+            {
+                this.Points = Points;
+>>>>>>> parent of a73e55a... 1
             }
         }
     }
@@ -70,6 +124,7 @@ namespace Objects1
 
     class Text : IDraw
     {
+<<<<<<< HEAD
         public int x; //point on the abscissa, in which the cursor is moved
         public int y = 33; //point on the ordinate axis in which the cursor is moved
         string text = "Give me life, give me love \nScarlet angel from above \nNot so low, not so high \nKeep it perfectly disguised";
@@ -78,11 +133,26 @@ namespace Objects1
             Console.SetCursorPosition(x, y); //move the cursor
             Console.Write("Text: \n{0}", text); //output a text
             Console.Write("\n------------------------------------"); //separation
+=======
+        protected Point Point1;
+        private String text;
+        public Text(Point Point1, String text)
+        {
+            this.Point1 = Point1;
+            this.text = text;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(Point1.x, 0); //move the cursor to this coordinates
+            Console.Write("Text: \n{0}", text); //output a text
+>>>>>>> parent of a73e55a... 1
         }
     }
 
 
     class DrawObjects
+<<<<<<< HEAD
     {
         public static void Iniciallization() //Method for work with object
         {
@@ -151,6 +221,38 @@ namespace Objects1
             Console.WriteLine("\n\n\nArray of objects:");
 
             DrawObjects.Iniciallization(); //call a method
+=======
+    {         
+        public static void Iniciallization(ref Polygon[] arrayofobjects) //Method for work with object
+        {
+            for (int i = 0; i < arrayofobjects.Length; i++)
+            {             
+                arrayofobjects[i].Draw();
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Program
+    {
+        static Point[] Points1 = { new Point(11, 22), new Point(33, 44), new Point(55, 66) }; //Initialize an array of points
+        static Point[] Points = { new Point(1, 2), new Point(3, 4), new Point(5, 6), new Point(7, 8), new Point(9, 10) };
+        static Point Point1 = new Point(10,0);
+
+        static void Main()
+        {
+            Polygon[] arrayofobjects = { new Polygon(Points), new Triangle(Points1)};//, new Text(Point1, "Give me life, give me love \nScarlet angel from above \nNot so low, not so high \nKeep it perfectly disguised") };
+
+            Console.WriteLine("Вывод в методе мэйн");
+            foreach (Polygon p in arrayofobjects)
+            {
+                p.Draw();
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Через метод вывода массива на экран");
+            DrawObjects.Iniciallization(ref arrayofobjects);
+>>>>>>> parent of a73e55a... 1
             Console.ReadKey();
         }
     }
@@ -158,4 +260,8 @@ namespace Objects1
 //9.Создать класс для описания многоугольников в общем и треугольника в частности. 
 //Также создать класс для описания текста. Все классы должны иметь метод вывода на экран. 
 //Создать отдельный класс для операций над объектами. В классе должен быть метод вывода массива объектов на экран. 
+<<<<<<< HEAD
 //Все объекты задавать в исходном коде (не меньше 3-х различных треугольников и двух текстов
+=======
+//Все объекты задавать в исходном коде
+>>>>>>> parent of a73e55a... 1
