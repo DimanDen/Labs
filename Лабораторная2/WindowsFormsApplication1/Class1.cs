dea87b37e2d99d8599bb5dd80c6ff51cff5a17be
DimanDen <diman.mymail@rambler.ru>
2014-06-09 20:28:52 +0400

    first step form

--- Лабораторная2/WindowsFormsApplication1/Class1.cs
+++ Лабораторная2/WindowsFormsApplication1/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Trajectory
+namespace WindowsFormsApplication1
 {
     public class Point
     {
@@ -19,12 +19,13 @@ namespace Trajectory
 
     public abstract class Trajectory // Траектория
     {
-        public abstract void Function(Point[] Points);
+        public abstract void Function(Point[] Points, Form1 Okno);
     }
 
-    class Polynomial : Trajectory  //Многочлен n-ого порядка. Имеет вид c0 + c1*x + c2*(x^2) + ...
+    public class Polynomial : Trajectory  //Многочлен n-ого порядка. Имеет вид c0 + c1*x + c2*(x^2) + ...
     {
-        protected double[] coefficients; //В массиве содержатся коэффициенты для определение многочлена той или иной степени
+        public int NumberOfSeries;
+        public double[] coefficients; //В массиве содержатся коэффициенты для определение многочлена той или иной степени
         //В данном методе определяется уравнение для нахождения значений фукнции. 
         //Например: если в массиве коэффициентов будет три элемента, значит степень многочлена будет второй. 
         //Следовательно мы имеем дело с параболой
@@ -38,7 +39,7 @@ namespace Trajectory
             set
             { }
         }
-        public override void Function(Point[] Points)
+        public override void Function(Point[] Points, Form1 Okno)
         {
             for (int i = 0; i < Points.Length; i++)
             {
@@ -46,37 +47,42 @@ namespace Trajectory
                 {
                     Points[i].y = Points[i].y + coefficients[j] * Math.Pow(Points[i].x, j);
                 }
-                Console.WriteLine("({0}; {1})", Points[i].x, Points[i].y);
+                //Okno.NewLines[NumberOfSeries][i+1] = "(" + Points[i].x.ToString() + "; " +  Points[i].y.ToString() + ")";
+               // Console.WriteLine("({0}; {1})", Points[i].x, Points[i].y); 
+                Okno.chart1.Series[NumberOfSeries].Points.AddXY(Points[i].x, Points[i].y);
             }
         }
     }
 
-    class Parabola : Polynomial //Парабола         
+    public class Parabola : Polynomial //Парабола         
     {
-        public Parabola(double[] coefficients) //constructor with a pointer to the object
+        public Parabola(double[] coefficients, int NumberOfSeries) //constructor with a pointer to the object
         {
             if (coefficients.Length == 3)
             {
                 this.coefficients = coefficients;
+                this.NumberOfSeries = NumberOfSeries;
             }
         }
     }
 
-    class Straight : Polynomial  //Прямая   
+    public class Straight : Polynomial  //Прямая   
     {
-        public Straight(double[] coefficients) //constructor with a pointer to the object
+        public Straight(double[] coefficients, int NumberOfSeries) //constructor with a pointer to the object
         {
             if (coefficients.Length == 2)
             {
                 this.coefficients = coefficients;
+                this.NumberOfSeries = NumberOfSeries;
             }
         }
     }
 
-    class TranscendentalCurves : Trajectory  // Трансцендентные кривые
+    public class TranscendentalCurves : Trajectory  // Трансцендентные кривые
     {
+        public int NumberOfSeries;
         protected double SinusoidFreeTerm; // свободный член для синусоиды
-        public override void Function(Point[] Points) //В данном методе находим значения функции. Функция уже будет выглядить как многочлен.
+        public override void Function(Point[] Points, Form1 Okno) //В данном методе находим значения функции. Функция уже будет выглядить как многочлен.
         {
             for (int i = 0; i < Points.Length; i++)
             {
@@ -84,25 +90,26 @@ namespace Trajectory
                 {
                     Points[i].y = Math.Sin(Points[i].x) + SinusoidFreeTerm;
                 }
-                Console.WriteLine("({0}; {1})", Points[i].x, Points[i].y);
+                Okno.chart1.Series[NumberOfSeries].Points.AddXY(Points[i].x, Points[i].y);
             }
         }
     }
 
-    class Sinusoid //Синусоида
+    public class Sinusoid //Синусоида
         : TranscendentalCurves
     {
-        public Sinusoid(double SinusoidFreeTerm) //constructor with a pointer to the object
+        public Sinusoid(double SinusoidFreeTerm, int NumberOfSeries) //constructor with a pointer to the object
         {
             this.SinusoidFreeTerm = SinusoidFreeTerm;
+            this.NumberOfSeries = NumberOfSeries;
         }
     }
 
 
-    class Collision //Столкновение
+    public class Collision //Столкновение
     {
         public Point[] PointsCollision = { new Point(0.12, 1.29), new Point(1, 1), new Point(3, 9), new Point(1.45, 3.91), new Point(2.78, 5.34) };
-        public void ChechCollision(Point[] Points)
+        public void ChechCollision(Point[] Points, Form1 Okno)
         {
             for (int i = 0; i < Points.GetLength(0); i++)
             {
@@ -112,47 +119,13 @@ namespace Trajectory
                     string str2 = Points[i].y.ToString();
                     string str3 = PointsCollision[j].x.ToString();
                     string str4 = PointsCollision[j].y.ToString();
-                    if (str1.Remove(4) == str3 && str2.Remove(4) == str4)
+                    if (str1.Remove(0) == str3 && str2.Remove(0) == str4)
                     {
                         Console.WriteLine("Произошлко столкновение с перпятствием в точке: ({0}; {1}) ", Points[i].x, Points[i].y);
                     }
-                }
+                    Okno.chart1.Series[3].Points.AddXY(PointsCollision[j].x, PointsCollision[j].y);
+                } 
             }
-        }
-    }
-
-    class Program
-    {
-        static void Main()
-        {
-            Point[] Points1 = { new Point(0.123, 0), new Point(1.456, 0), new Point(2.789, 0) };
-            double[] coefficients1 = new double[3] { 1, 2, 3 };
-            Point[] Points2 = { new Point(0.123, 0), new Point(1.456, 0), new Point(2.789, 0) };
-            double[] coefficients2 = new double[2] { 1, 2 };
-            Point[] Points3 = { new Point(0.123, 0), new Point(1.456, 0), new Point(2.789, 0) };
-            double SinusoidFreeTerm1 = 5.0;
-
-            Console.WriteLine("Парабола");
-            Parabola parabola = new Parabola(coefficients1);
-            parabola.Function(Points1);
-
-            Console.WriteLine("\nПрямая");
-            Straight straight = new Straight(coefficients2);
-            straight.Function(Points2);
-
-            Console.WriteLine("\nСинусоида");
-            Sinusoid sinusoid = new Sinusoid(SinusoidFreeTerm1);
-            sinusoid.Function(Points3);
-
-            Console.WriteLine("\nИнформация о столкновении с препятствиями:");
-            Collision c = new Collision();
-            Console.WriteLine("\nНаличие столкновения у параболы:");
-            c.ChechCollision(Points1);
-            Console.WriteLine("\nНаличие столкновения у прямой:");
-            c.ChechCollision(Points2);
-            Console.WriteLine("\nНаличие столкновения у синусоиды:");
-            c.ChechCollision(Points3);
-            Console.ReadKey();
         }
     }
 }
---
+++ Лабораторная2/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 335);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.method1);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(548, 15);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Парабола";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Прямая";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.Name = "Синусоида";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series4.Legend = "Legend1";
+            series4.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series4.Name = "Посторонние объекты";
+            series4.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(646, 443);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 293);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(182, 13);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(168, 293);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(356, 15);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(174, 291);
+            this.textBox3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1193, 470);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        public System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.TextBox textBox2;
+        public System.Windows.Forms.TextBox textBox3;
+    }
+}
+
